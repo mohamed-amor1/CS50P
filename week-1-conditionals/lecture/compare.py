3a3ba3abb2ebc9dffff0349fed5ef7f3a3396ae5
purplegem903 <82189117+purplegem903@users.noreply.github.com>
2023-04-10 00:31:38 +0100

    Python code for comparing two numbers.

---
+++ week-1-conditionals/lecture/compare.py
@@ -0,0 +1,11 @@
+# Get user input for x and y
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+# Compare the values of x and y using if-elif statements
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
