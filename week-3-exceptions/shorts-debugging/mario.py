6e8dbf549bf10b32a7c36af3fbe9949d862e3a2e
purplegem903 <82189117+purplegem903@users.noreply.github.com>
2023-04-15 01:05:04 +0100

    debugging

--- week-3-exceptions/shorts-debugging/mario.py
+++ week-3-exceptions/shorts-debugging/mario.py
@@ -1,11 +1,18 @@
 def main():
     height = int(input("Height: "))
     pyramid(height)
+    pyramid_2(height)
 
 
 def pyramid(n):
     for i in range(n):
         print("#" * (i + 1))
+
+
+def pyramid_2(n):  # define a function called 'pyramid_2' that takes one parameter 'n'
+    for i in range(n):  # loop through numbers 0 to n-1
+        # on each iteration, print a string with n-i-1 spaces " " followed by i+1 hash "#" characters
+        print(" " * (n - i - 1) + "#" * (i + 1))
 
 
 if __name__ == "__main__":
